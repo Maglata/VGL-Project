47e6d39fc600511fdde6698203464ee16bad7f35
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2023-05-27 21:25:04 +0300

    Adjusted header and fixed arrows

--- front-end/js/carousel.js
+++ front-end/js/carousel.js
@@ -5,49 +5,36 @@ const springcards = springcarousel.querySelectorAll("li");
 const arrowright = document.querySelector(".arrow-button--right")
 const arrowleft = document.querySelector(".arrow-button--left")
 const cardWidth = springcards[0].offsetWidth;
+let springcarouselScrollPos = springcarousel.scrollLeft;
 
 arrowright.addEventListener("click", () => {
-  scrollCarousel(springcarousel, cardWidth);
-  checkScrollLimits(springcarousel, cardWidth, arrowleft, arrowright);
+  move(springcarousel,cardWidth);
+  checkScrollLimits(springcarousel, arrowleft, arrowright);
 });
 
 arrowleft.addEventListener("click", () => {
-  scrollCarousel(springcarousel, -cardWidth);
-  checkScrollLimits(springcarousel, -cardWidth, arrowleft, arrowright);
+  move(springcarousel,-cardWidth);
+  checkScrollLimits(springcarousel, arrowleft, arrowright);
 });
 
 // Carousel Scrolling
-function scrollCarousel(carousel, amount) {
-  const scrollLeftPos = carousel.scrollLeft;
-  const duration = 500; // Adjust this value to control the scrolling duration (in milliseconds)
-  const startTime = performance.now();
-
-  function scrollStep(timestamp) {
-    const elapsed = timestamp - startTime;
-    const progress = Math.min(elapsed / duration, 1);
-    const scrollDistance = amount * progress; // Calculate scroll distance based on the amount parameter
-    carousel.scrollLeft = scrollLeftPos + scrollDistance;
-
-    if (progress < 1) {
-      window.requestAnimationFrame(scrollStep);
-    }
-  }
-
-  window.requestAnimationFrame(scrollStep);
+function move(carousel, amount) {
+  carousel.scrollLeft += amount;
+  springcarouselScrollPos = carousel.scrollLeft += amount;
 }
+
 // Scroll Checking
-function checkScrollLimits(carousel, cardWidth, leftArrow, rightArrow) {
+function checkScrollLimits(carousel, leftArrow, rightArrow) {
   const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
-  const scrollLeftPos = carousel.scrollLeft;
-  console.log(maxScrollLeft, scrollLeftPos);
 
-  if (scrollLeftPos > 0) {
-    leftArrow.style.display = "flex";
-  } else {
+  const scrollLeftPos = springcarouselScrollPos;
+
+  if (scrollLeftPos <= 0) {
     leftArrow.style.display = "none";
+  } else{
+    leftArrow.style.display = "flex";
   }
-
-  if (scrollLeftPos < maxScrollLeft - cardWidth) {
+  if (scrollLeftPos < maxScrollLeft) {
     rightArrow.style.display = "flex";
   } else {
     rightArrow.style.display = "none";
