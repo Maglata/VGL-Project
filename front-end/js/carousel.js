b0aca69dd990ff01c879274b7120efce88b4b492
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2023-05-31 19:58:20 +0300

    Added pop-up for adding games to the list

--- front-end/js/carousel.js
+++ front-end/js/carousel.js
@@ -2,6 +2,8 @@
 const sections = document.querySelectorAll("section");
 
 sections.forEach((section,index) =>{
+
+  // Skipping the first Carousel for Most Played
   if(index === 0){
     return;
   }
@@ -12,7 +14,8 @@ sections.forEach((section,index) =>{
   let carouselScrollPos = carousel.scrollLeft;
 
   // Define the URL of the icon image
-  const iconImageUrl = 'imgs/misc/icon-game-add.png';
+  const iconAddImageUrl = 'imgs/misc/icon-game-add.png';
+  const iconRemoveImageUrl = 'imgs/misc/icon-game-remove.png';
 
   // Arrow Scrolling 
   arrowright.addEventListener("click", () => {
@@ -26,7 +29,6 @@ sections.forEach((section,index) =>{
 
   // Hover Animation for each card
   cards.forEach(card =>{
-
     card.addEventListener("mouseover", () => {
       card.style.transform = "scale(1.1)";
       card.style.transition = "0.3s ease";
@@ -40,28 +42,51 @@ sections.forEach((section,index) =>{
       iconImage.style.opacity = '0';
     });
 
-    // Create a mask element for each card
-    const maskElement = document.createElement('div');
-    maskElement.className = 'card-mask';
+      // Create a mask element for each card
+      const maskElement = document.createElement('div');
+      maskElement.className = 'card-mask';
 
-    // Append the mask element to the card
-    card.appendChild(maskElement);
+      // Append the mask element to the card
+      card.appendChild(maskElement);
 
-    // Create the icon image element
-    const iconImage = document.createElement('img');
-    iconImage.src = iconImageUrl;
-    iconImage.classList.add('card-icon');
+      // Create the icon image element
+      const iconImage = document.createElement('img');
+      iconImage.src = iconAddImageUrl;
+      iconImage.classList.add('card-icon');
   
-    // Append the icon image element to the card
-    card.appendChild(iconImage);
+      // Append the icon image element to the card
+      card.appendChild(iconImage);
 
-    // Add click event handler for the icon image
-    iconImage.addEventListener('click', () => {
-    // Perform your desired function here
-    console.log('Icon clicked!');
+      // Add click event handler for the icon image
+      iconImage.addEventListener('click', () => {
+      const popup = document.createElement('div');
+
+      // Extract the relative image path
+      const splitParts = iconImage.src.split('/'); // Split the URL by '/'
+      const relativeImagePath = splitParts.slice(-3).join('/'); // Extract the last 3 parts and join them back with '/'
+
+       // If the item hasn't been added yet
+      if(relativeImagePath === iconAddImageUrl){
+        popup.textContent = 'Game added';
+        popup.className = 'popup-added';
+        iconImage.src = iconRemoveImageUrl;
+      } // If the item has been added
+      else{
+        popup.textContent = 'Game removed';
+        popup.className = 'popup-removed';
+        iconImage.src = iconAddImageUrl;
+      }
+
+      // Append the popup element to the card
+      card.appendChild(popup);
+
+      // Remove the popup after a second
+      setTimeout(() => {
+        card.removeChild(popup);
+      }, 1000);
   });
-
   })
+
 })
 
 // Function to add the mask effect
