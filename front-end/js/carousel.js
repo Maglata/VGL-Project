e9b616dcedb1accce8a5d56841b9338e36607101
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2023-05-31 17:07:51 +0300

    Adjusted img quality

--- front-end/js/carousel.js
+++ front-end/js/carousel.js
@@ -103,7 +103,7 @@ let counter = 1;
 setInterval(function(){
   document.getElementById('radio' + counter).checked = true;
   counter++;
-  if(counter > 4){
+  if(counter > 5){
     counter = 1;
   }
 }, 5000);
