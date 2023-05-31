963f690d20bfd46eebf39b94ddcb141c6ca2ed82
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2023-05-31 16:59:15 +0300

    Added dynamic icon and shade for cards

--- front-end/js/carousel.js
+++ front-end/js/carousel.js
@@ -11,6 +11,9 @@ sections.forEach((section,index) =>{
   const arrowleft = section.querySelector(".arrow-button--left")
   let carouselScrollPos = carousel.scrollLeft;
 
+  // Define the URL of the icon image
+  const iconImageUrl = 'imgs/misc/icon-game-add.png';
+
   // Arrow Scrolling 
   arrowright.addEventListener("click", () => {
     carouselScrollPos = move(carousel,carousel.clientWidth);
@@ -27,13 +30,51 @@ sections.forEach((section,index) =>{
     card.addEventListener("mouseover", () => {
       card.style.transform = "scale(1.1)";
       card.style.transition = "0.3s ease";
+      addMask(card);
+      iconImage.style.opacity = '1';
     });
   
     card.addEventListener("mouseleave", () => {
       card.style.transform = "scale(1)";
+      removeMask(card);
+      iconImage.style.opacity = '0';
     });
+
+    // Create a mask element for each card
+    const maskElement = document.createElement('div');
+    maskElement.className = 'card-mask';
+
+    // Append the mask element to the card
+    card.appendChild(maskElement);
+
+    // Create the icon image element
+    const iconImage = document.createElement('img');
+    iconImage.src = iconImageUrl;
+    iconImage.classList.add('card-icon');
+  
+    // Append the icon image element to the card
+    card.appendChild(iconImage);
+
+    // Add click event handler for the icon image
+    iconImage.addEventListener('click', () => {
+    // Perform your desired function here
+    console.log('Icon clicked!');
+  });
+
   })
 })
+
+// Function to add the mask effect
+function addMask(card) {
+  const maskElement = card.querySelector('.card-mask');
+  maskElement.style.opacity = '1';
+}
+
+// Function to remove the mask effect
+function removeMask(card) {
+  const maskElement = card.querySelector('.card-mask');
+  maskElement.style.opacity = '0';
+}
 
 // Carousel Scrolling
 function move(carousel, amount) {
